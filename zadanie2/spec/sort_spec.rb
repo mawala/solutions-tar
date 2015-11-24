a33c0ed423e8103a6f7c84cc64a512552e14b3f2
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2015-11-24 23:07:27 +0100

    Zmiana testow-uzycie context

--- zadanie2/spec/sort_spec.rb
+++ zadanie2/spec/sort_spec.rb
@@ -5,12 +5,29 @@ require 'sort'
 
 RSpec.describe 'sort.rb' do
   describe '#qsort' do
+
     it 'should be defined' do
       expect { qsort([1, 2, 3]) }.not_to raise_error
     end
-    it 'returns the sorted array' do
-      expect(qsort([2, 1, 9])).to be_a_kind_of(Array)
-      expect(qsort([9, -1, 4])).to eq([-1, 4, 9])
+
+    context 'with no argument' do
+      it { expect { qsort() }.to raise_error(ArgumentError) }
+    end
+
+    context 'with no array' do
+      it { expect { qsort(1) }.to raise_error(NoMethodError) }
+    end
+
+    context 'with empty array' do 
+      it { expect(qsort([])).to eq([]) }
+    end
+
+    context 'with array' do
+      it 'should return the sorted array' do
+        expect(qsort([2, 1, 9])).to be_a_kind_of(Array)
+        expect(qsort([9.3, -1, 4])).to eq([-1, 4, 9.3])
+        expect(qsort(%w[one two three])).to eq(%w[one three two])
+      end
     end
   end
 end
